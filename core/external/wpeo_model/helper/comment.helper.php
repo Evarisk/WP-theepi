8d39ab282159b314d6bd95bb1954b8a741c8f274
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-07-04 10:44:53 +0200

    Add submodule

---
+++ core/external/wpeo_model/helper/comment.helper.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Fonctions "helper" des commentaires
+ *
+ * @author Jimmy Latour <dev@eoxia.com>
+ * @since 1.0.0.0
+ * @version 1.3.0.0
+ * @copyright 2015-2017
+ * @package wpeo_model
+ * @subpackage helper
+ */
+
+namespace eoxia;
+
+if ( ! defined( 'ABSPATH' ) ) { exit; }
+
+if ( ! function_exists( '\eoxia\construct_current_date' ) ) {
+	/**
+	 * Met la date au bon format. Si la date n'existe pas, prend la date du jour.
+	 *
+	 * @param  Object $data L'objet courant.
+	 *
+	 * @return Object       L'objet avec la date.
+	 *
+	 * @since 1.0.0.0
+	 * @version 1.3.0.0
+	 */
+	function construct_current_date( $data ) {
+		$data->date = ! empty( $data->date ) ? date( 'd/m/Y', strtotime( $data->date ) ) : current_time( 'd/m/Y' );
+
+		return $data;
+	}
+}
+
+if ( ! function_exists( '\eoxia\convert_date' ) ) {
+	/**
+	 * Convertie la date au format français dd/mm/yy en format SQL
+	 *
+	 * @param  object $data Les donnnées du modèle.
+	 * @return object       Les donnnées du modèle avec la date au format SQL
+	 *
+	 * @since 1.0.0.0
+	 * @version 1.3.0.0
+	 */
+	function convert_date( $data ) {
+		if ( ! empty( $data ) && ! empty( $data->date ) ) {
+			$data->date = date( 'Y-m-d', strtotime( str_replace( '/', '-', $data->date ) ) );
+		}
+
+		return $data;
+	}
+}
