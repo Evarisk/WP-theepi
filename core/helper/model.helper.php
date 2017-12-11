4410ae0fceb2c92d4bbd6cbacea8379e23bdc9c7
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-12-11 16:52:34 +0100

    0.2.0: Update to WordPress standard

--- core/helper/model.helper.php
+++ core/helper/model.helper.php
@@ -1,8 +1,12 @@
 <?php
 /**
- * Les fonctions helpers des modèles
+ * Handle main helpers.
  *
- * @package Evarisk\digirisk-epi
+ * @author Jimmy Latour <jimmy@evarisk.com>
+ * @since 0.1.0
+ * @version 0.2.0
+ * @copyright 2017 Evarisk
+ * @package TheEPI
  */
 
 namespace theepi;
@@ -14,11 +18,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Construit l'identifiant unique d'un modèle
  *
+ * @since 0.1.0
+ * @version 0.2.0
+ *
  * @param  object $data Les données du modèle.
  * @return object       Les données du modèle avec l'identifiant
  */
 function construct_identifier( $data ) {
-	$model_name = get_class( $data );
+	$model_name      = get_class( $data );
 	$controller_name = str_replace( 'model', 'class', $model_name );
 	$controller_name = str_replace( 'Model', 'Class', $controller_name );
 	$next_identifier = get_last_unique_key( $controller_name );
@@ -37,8 +44,8 @@ function construct_identifier( $data ) {
 /**
  * Remplaces l'identifiant du modèle par l'identifiant personnalisé qui se trouve dans la BDD
  *
- * @since 6.0.0
- * @version 6.3.0
+ * @since 0.1.0
+ * @version 0.1.0
  *
  * @param  object $data Les données du modèle.
  * @return object       Les données du modèle avec l'identifiant personnalisé
@@ -47,90 +54,20 @@ function get_identifier( $data ) {
 	return $data;
 }
 
-
-/**
- * Convertie la date au format SQL vers le format français
- *
- * @param  object $data Les donnnées du modèle.
- * @return object       Les donnnées du modèle avec la date au format SQL
- */
-function convert_date_display( $data ) {
-	$data->date = ! empty( $data->date ) ? date( 'd/m/Y', strtotime( $data->date ) ) : current_time( 'd/m/Y' );
-
-	return $data;
-}
-
-/**
- * Construit les initiales des utilisateurs
- *
- * @param  \digi\User_class $user Les données de l'utilisateur.
- * @return \digi\User_class       Les données de l'utilisateur avec les intiales
- */
-function build_user_initial( $user ) {
-	$initial = '';
-
-	if ( ! empty( $user->firstname ) ) {
-		$initial .= substr( $user->firstname, 0, 1 );
-	}
-	if ( ! empty( $user->lastname ) ) {
-		$initial .= substr( $user->lastname, 0, 1 );
-	}
-
-	if ( empty( $initial ) ) {
-		if ( ! empty( $user->login ) ) {
-			$initial .= substr( $user->login, 0, 1 );
-		}
-	}
-
-	$user->initial = $initial;
-
-	return $user;
-}
-
-/**
- * Initialise le tableau avec les couleurs possibles pour l'utilisateur
- *
- * @param  \digi\User_class $user Les données de l'utilisateur.
- * @return \digi\User_class       Les données de l'utilisateur avec la couleur.
- */
-function build_avatar_color( $user ) {
-	$avatar_color = array(
-		'e9ad4f',
-		'50a1ed',
-		'e05353',
-		'e454a2',
-		'47e58e',
-		'734fe9',
-	);
-
-	$user->avatar_color = $avatar_color[ array_rand( $avatar_color, 1 ) ];
-
-	return $user;
-}
-
-/**
- * Force la couleur d'un tuilisateur
- *
- * @param  User_Class $user Les données de l'utilisateur.
- * @return User_Class       Les données de l'utilisateur avec la couleur forcé.
- */
-function force_avatar_color( $user ) {
-	$user->avatar_color = '50a1ed';
-	return $user;
-}
-
 /**
  * Renvoie la dernière clé unique selon le type de l'élement
  *
- * @since 6.3.1
- * @version 6.3.1
+ * @since 0.1.0
+ * @version 0.1.0
  *
- * @param  string $controller Le controller.
- * @return int             		L'identifiant unique
+ * @param string $controller   Le controller.
+ *
+ * @return integer             L'identifiant unique
  */
 function get_last_unique_key( $controller ) {
 	$element_type = $controller::g()->get_post_type();
-	$wp_type = $controller::g()->get_identifier_helper();
+	$wp_type      = $controller::g()->get_identifier_helper();
+
 	if ( empty( $wp_type ) || empty( $element_type ) || ! is_string( $wp_type ) || ! is_string( $element_type ) ) {
 		return false;
 	}
@@ -143,7 +80,8 @@ function get_last_unique_key( $controller ) {
 				FROM {$wpdb->postmeta} AS PM
 					INNER JOIN {$wpdb->posts} AS P ON ( P.ID = PM.post_id )
 				WHERE PM.meta_key = %s
-					AND P.post_type = %s", '_wpdigi_unique_key', $element_type );
+					AND P.post_type = %s", '_theepi_unique_key', $element_type );
+
 		break;
 
 		case 'comment':
@@ -152,14 +90,14 @@ function get_last_unique_key( $controller ) {
 				FROM {$wpdb->commentmeta} AS CM
 					INNER JOIN {$wpdb->comments} AS C ON ( C.comment_ID = CM.comment_id )
 				WHERE CM.meta_key = %s
-					AND C.comment_type = %s", '_wpdigi_unique_key', $element_type );
+					AND C.comment_type = %s", '_theepi_unique_key', $element_type );
 		break;
 
 		case 'user':
 			$query = $wpdb->prepare(
 				"SELECT max( UM.meta_value + 0 )
 				FROM {$wpdb->usermeta} AS UM
-				WHERE UM.meta_key = %s", '_wpdigi_unique_key' );
+				WHERE UM.meta_key = %s", '_theepi_unique_key' );
 		break;
 
 		case 'term':
@@ -167,7 +105,7 @@ function get_last_unique_key( $controller ) {
 				"SELECT max( TM.meta_value + 0 )
 				FROM {$wpdb->term_taxonomy} AS T
 					INNER JOIN {$wpdb->termmeta} AS TM ON ( T.term_id = TM.term_id )
-				WHERE TM.meta_key = %s AND T.taxonomy=%s", '_wpdigi_unique_key', $element_type );
+				WHERE TM.meta_key = %s AND T.taxonomy=%s", '_theepi_unique_key', $element_type );
 		break;
 	}
 
